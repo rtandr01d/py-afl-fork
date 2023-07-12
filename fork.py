5e712f7e8829c14ef7f29bd436b4d411ac72caed
rtandr01d <rtandr01d@protonmail.com>
2023-07-12 15:50:17 -0500

    Testing

--- fork.py
+++ fork.py
@@ -5,7 +5,7 @@ import argparse
 parser = argparse.ArgumentParser(description="AFL++ Multi Process")
 parser.add_argument('-i', '--input', help='Seed Corpus')
 parser.add_argument('-o', '--output', help='Output Dir')
-parser.add_argument("FILE")
+parser.add_argument("FILE", nargs='*')
 
 cpu_count = multiprocessing.cpu_count()
 
@@ -13,6 +13,7 @@ args = parser.parse_args()
 
 for i in range(cpu_count):
   if (i == 0):
-    subprocess.run(['afl-fuzz', '-i', args.input, '-o', args.output, '-s', '123', '--', args.FILE, '@@'], shell=True, check=True)
+    subprocess.Popen(["afl-fuzz", "-m", "none", "-D", "-i", args.input, "-o", args.output, "-M", "Master", "-b", str(i), "-s", "123", "--", args.FILE, "@@"])
   else:
-    print("Slave" + str(i))
+    slavenum = "slave" + str(i)
+    subprocess.Popen(["afl-fuzz", "-m", "none", "-i", args.input, "-o", args.output, "-S", slavenum, "-b", str(i), "--", args.FILE, "@@"])

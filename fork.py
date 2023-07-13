8e3a38b7eac5f3589e17d27664803ad60cfbc77d
rtandr01d <rtandr01d@protonmail.com>
2023-07-12 22:34:06 -0500

    Fork Fixed

--- fork.py
+++ fork.py
@@ -5,15 +5,21 @@ import argparse
 parser = argparse.ArgumentParser(description="AFL++ Multi Process")
 parser.add_argument('-i', '--input', help='Seed Corpus')
 parser.add_argument('-o', '--output', help='Output Dir')
-parser.add_argument("FILE", nargs='*')
+parser.add_argument("FILE")
+parser.add_argument ('--flags', nargs="?", help='Flags for target binary', required=False)
 
 cpu_count = multiprocessing.cpu_count()
 
 args = parser.parse_args()
 
+#' '.join(args.flags)
+
+
 for i in range(cpu_count):
   if (i == 0):
-    subprocess.Popen(["afl-fuzz", "-m", "none", "-D", "-i", args.input, "-o", args.output, "-M", "Master", "-b", str(i), "-s", "123", "--", args.FILE, "@@"])
-  else:
-    slavenum = "slave" + str(i)
-    subprocess.Popen(["afl-fuzz", "-m", "none", "-i", args.input, "-o", args.output, "-S", slavenum, "-b", str(i), "--", args.FILE, "@@"])
+    subprocess.Popen(["afl-fuzz", "-m", "none", "-D", "-i", args.input, "-o", args.output, "-M", "Master", "-b", str(i), "-s", "123", "--", args.FILE, args.flags, "@@"])
+    #print("afl-fuzz -m none -D -i " + args.input + " -o " + args.output + "-M Master -b " + str(i) + " -s 123 -- " + args.FILE + " " + args.flags + " @@")
+  #else:
+    #slavenum = "slave" + str(i)
+    #subprocess.Popen(["afl-fuzz", "-m", "none", "-i", args.input, "-o", args.output, "-S", slavenum, "-b", str(i), "--", args.FILE, "@@"])
+
